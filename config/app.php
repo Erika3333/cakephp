5753549c2b587ddb60dc5f86dbe78ca0f2a60b28
erika <tedokon.erika@gmail.com>
2021-10-11 15:09:54 +0900

    heroku Database接続設定

--- config/app.php
+++ config/app.php
@@ -280,9 +280,14 @@ return [
             //'username' => 'my_app',
             //'password' => 'secret',
             //'database' => 'my_app',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'db',
+
+            // 'username' => 'my_app',
+            // 'password' => 'secret',
+            // 'database' => 'db',
+
+            'username' => 'nnhbbsydflwdux',
+            'password' => '1bdb61f535036ff510d4e4bfeafa2b49f23395f48a7afd22abf1a56982f5ff18',
+            'database' => '5432/d9a194016lcubo`',
 
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
